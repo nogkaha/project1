4b7d8ad15121f0c5854f9b02ed534ee8f48d0ed9
nogkaha <nogkaha@gmail.com>
2017-01-27 22:00:48 +0100

    draft file

---
+++ drafts.R
@@ -0,0 +1,5 @@
+#DRAFTS
+#dealing with the numeric data
+#numeric_index<-names(select(rawfile,networking1:networking3,
+#            art_scene1:art_scene4,
+#           city_general1:city_general7))
